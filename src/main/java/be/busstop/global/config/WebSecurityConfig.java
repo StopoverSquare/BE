417d6bdbe82a0239ce2e64ae46fc75d89f7f8bc6
Mi <mi_aa_@naver.com>
2023-12-01 12:00:59 +0900

    fix: Password Encode 추가

--- src/main/java/be/busstop/global/config/WebSecurityConfig.java
+++ src/main/java/be/busstop/global/config/WebSecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -69,5 +71,9 @@ public class WebSecurityConfig{
         http.addFilterBefore(corsConfig.corsFilter(), JwtAuthorizationFilter.class);
 
         return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
