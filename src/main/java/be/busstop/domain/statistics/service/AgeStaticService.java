e7758f3fa5db2eaa48927fd4fc9fd9bb87d83a46
Mi <mi_aa_@naver.com>
2024-01-04 15:00:07 +0900

    feat: 나이 통계 구현

--- src/main/java/be/busstop/domain/statistics/service/AgeStaticService.java
+++ src/main/java/be/busstop/domain/statistics/service/AgeStaticService.java
@@ -1,5 +1,6 @@
 package be.busstop.domain.statistics.service;
 
+import be.busstop.domain.statistics.dto.AgeStaticResponseDto;
 import be.busstop.domain.statistics.entity.AgeStatic;
 import be.busstop.domain.statistics.repository.AgeStaticRepository;
 import be.busstop.domain.user.entity.User;
@@ -31,5 +32,35 @@ public class AgeStaticService {
             ageStatic.plusCnt(age);
         }
         ageStaticRepository.save(ageStatic);
+    }
+
+    public AgeStaticResponseDto getAllAgeStatic(){
+        List<AgeStatic> ageStatics = ageStaticRepository.findAll();
+        Long tenCnt = 0L;
+        Long twentyCnt = 0L;
+        Long thirtyCnt = 0L;
+        Long fortyCnt = 0L;
+        Long fiftyCnt = 0L;
+        Long sixtyCnt = 0L;
+        Long ageEtcCnt = 0L;
+        for(AgeStatic ageStatic : ageStatics){
+            tenCnt += ageStatic.getTenCnt();
+            twentyCnt += ageStatic.getTwentyCnt();
+            thirtyCnt += ageStatic.getThirtyCnt();
+            fortyCnt += ageStatic.getFortyCnt();
+            fiftyCnt += ageStatic.getFiftyCnt();
+            sixtyCnt += ageStatic.getSixtyCnt();
+            ageEtcCnt += ageStatic.getEtcCnt();
+        }
+
+        return AgeStaticResponseDto.builder()
+                .tenCnt(tenCnt)
+                .twentyCnt(twentyCnt)
+                .thirtyCnt(thirtyCnt)
+                .fortyCnt(fortyCnt)
+                .fiftyCnt(fiftyCnt)
+                .sixtyCnt(sixtyCnt)
+                .ageEtcCnt(ageEtcCnt)
+                .build();
     }
 }
